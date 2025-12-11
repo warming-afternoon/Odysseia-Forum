4e637ff8c67e65b7ea40ad9af2f11b851956564f
shimmerDay233 <97532589+warming-afternoon@users.noreply.github.com>
2025-12-11 20:40:35 +0800

    fix: 迁移脚本

--- src/shared/models/__init__.py
+++ src/shared/models/__init__.py
@@ -10,6 +10,8 @@ from .author import Author
 from .thread_follow import ThreadFollow
 from .banner_application import BannerApplication, BannerCarousel, BannerWaitlist
 from .user_collection import UserCollection
+from .booklist import Booklist
+from .booklist_item import BooklistItem
 
 # 这一行是为了让 Alembic/SQLModel 能够发现所有模型
 __all__ = [
@@ -27,4 +29,6 @@ __all__ = [
     "BannerCarousel",
     "BannerWaitlist",
     "UserCollection",
+    "Booklist",
+    "BooklistItem",
 ]
